d63fb5bbb8c5da5ad1867bb0bab1d099956f4208
JeonghoPark <pjhpjh0805@naver.com>
2023-05-14 23:15:08 +0900

    Problem 4.5

---
+++ src/Problem4_5.java
@@ -0,0 +1,28 @@
+import structure.TreeNode;
+
+/**
+ * BST 검증하기
+ */
+public class Problem4_5 {
+    static int prevNumber = Integer.MIN_VALUE;
+    public boolean isBst(TreeNode root) {
+        if (root == null) {
+            return true;
+        }
+
+        if (!isBst(root.left)) {
+            return false;
+        }
+
+        if (prevNumber != Integer.MIN_VALUE && root.val < prevNumber) {
+            return false;
+        }
+        prevNumber = root.val;
+
+        if (!isBst(root.right)) {
+            return false;
+        }
+
+        return true;
+    }
+}
