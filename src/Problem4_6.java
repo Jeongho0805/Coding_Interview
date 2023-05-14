90b31881ce55708622de6576d15cdcfcc6d7fc47
JeonghoPark <pjhpjh0805@naver.com>
2023-05-14 23:49:34 +0900

    Problem 4.6

---
+++ src/Problem4_6.java
@@ -0,0 +1,26 @@
+import structure.TreeNode;
+
+/**
+ * 중위 순회 기준 다음 노드 찾기
+ */
+public class Problem4_6 {
+    public TreeNode findNextNode(TreeNode node) {
+        if (node.right != null) {
+            return findLeftEndNod(node.right);
+        }
+        TreeNode cur = node;
+        TreeNode parent = node.parent;
+        while (parent != null && parent.left != cur) {
+            cur = parent;
+            parent = parent.parent;
+        }
+        return parent;
+    }
+
+    private TreeNode findLeftEndNod(TreeNode node) {
+        while (node.left != null) {
+            node = node.left;
+        }
+        return node;
+    }
+}
