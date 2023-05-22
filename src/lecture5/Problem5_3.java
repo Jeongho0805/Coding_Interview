d5567423bec9b0fe4249caddbca31afad614a281
JeonghoPark <pjhpjh0805@naver.com>
2023-05-22 15:25:30 +0900

    fix Problem5_3

--- src/lecture5/Problem5_3.java
+++ src/lecture5/Problem5_3.java
@@ -6,10 +6,20 @@ import java.util.StringTokenizer;
 
 /**
  * 어떤 정수의 비트 하나를 0에서 1로 바꿀 수 있다. 1이 연속으로 나올 수 있는 가장 긴 길이를 구해라
+ * cf : 11100111일 경우 고려 하지 못함
  */
 public class Problem5_3 {
     public static int reverseInteger(int input) {
+        String binaryOriginal = Integer.toBinaryString(input);
         String binary = Integer.toBinaryString(~input);
+        StringBuilder sb = new StringBuilder();
+        int diff = binaryOriginal.length() - binary.length();
+        while (diff -- > 0) {
+            sb.append("0");
+        }
+        sb.append(binary);
+        binary = sb.toString();
+
         StringTokenizer st = new StringTokenizer(binary, "1");
 
         List<Integer> lengthList = new ArrayList<>();
